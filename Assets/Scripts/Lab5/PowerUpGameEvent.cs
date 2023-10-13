c87b69dea72744253e8bbf5f19bc39e7ba75860a
Paprika0827 <xiongyang154@gmail.com>
2023-10-13 12:31:19 +0800

    lab5

---
+++ Assets/Scripts/Lab5/PowerUpGameEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// no arguments
+[CreateAssetMenu(fileName = "PowerUpGameEvent", menuName = "ScriptableObjects/PowerUpGameEvent", order = 5)]
+public class PowerUpGameEvent : GameEvent<IPowerup>
+{
+ // leave empty
+}
